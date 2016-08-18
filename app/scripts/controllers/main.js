27dfb49d17e0247193db6c08c62e69cba7a893bf
Juan Muñoz <alexx44@users.noreply.github.com>
2016-08-18 17:25:28 -0300

    Carrussel y menu nuevo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,48 @@
  * Controller of the prototipoApp
  */
 angular.module('prototipoApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+  var baseURL='http://lorempixel.com/960/450/';
+  $scope.setInterval=5000;
+
+  $scope.slides = [
+   {
+         title:'Aprende a mantenerte en forma',
+         image:baseURL+'sports/',
+         text:'¡Practica algún deporte todos los días'
+   },
+   {
+         title:'Buena alimentación',
+         image:baseURL+'food/',
+         text:'¡La importancia de frutas y verduras en la dieta!'
+   },
+   {
+         title:'En contacto con la naturaleza',
+         image:baseURL+'nature/',
+         text:'¡Mantente en armonía con la naturaleza!'
+   }
+
+    ]; 
+// Modelo para los tres bloques de contenidos
+var baseURL='http://lorempixel.com/200/200/'
+$scope.contenido=[
+{
+   img:baseURL+'people',
+   title:'Sobre Nosotros',
+   sumario:'Somos una empresa comprometida con la vida sana'
+},
+{
+   img:baseURL+'business',
+   title:'Nuestros Servicios',
+   sumario:'Ofrecemos asesoría profesional para mantenerse Sanos, buena alimentación, cuales son los mejores ejercicios para tí, etc.'
+},
+{
+
+   img:baseURL+'transport',
+   title:'Contáctanos',
+   sumario:'#333, Buena vida Online, Plaza Central, Durance, Zip-432167'
+}
+]
+
+    
   });
